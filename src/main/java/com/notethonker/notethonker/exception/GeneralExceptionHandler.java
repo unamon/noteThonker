50068c4b226c7735dbf155e21f4505989cdd2906
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-01-30 17:05:48 -0300

    ExceptionHandler

---
+++ src/main/java/com/notethonker/notethonker/exception/GeneralExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.notethonker.notethonker.exception;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import io.micrometer.common.lang.Nullable;
+
+@ControllerAdvice
+public class GeneralExceptionHandler extends ResponseEntityExceptionHandler{
+    
+    @Override 
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+        HttpHeaders headers, HttpStatusCode status, WebRequest request) { 
+            return super.handleMethodArgumentNotValid(ex, headers, HttpStatus.UNPROCESSABLE_ENTITY, request);
+        }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    private ResponseEntity<Object> handleBadRequest(EmptyResultDataAccessException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(ex.getMessage());
+    }
+}
