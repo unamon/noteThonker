62b6c4ec12cd91a5d51608e529c21b349fad9c1e
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2023-02-03 17:49:16 -0300

    repository tests

--- src/test/java/com/notethonker/notethonker/common/NoteConstants.java
+++ src/test/java/com/notethonker/notethonker/common/NoteConstants.java
@@ -6,5 +6,6 @@ public class NoteConstants {
     public static final Note NOTE = new Note("title", "content",null, null);
     public static final Note INVALID_NOTE = new Note("", "",null, null);
     public static final Note FULL_NOTE = new Note(1L, "title", "content",null, null);
+    public static final Note EMPTY_NOTE = new Note();
 
 }
---
+++ src/test/java/com/notethonker/notethonker/domain/NoteRespositoryTest.java
@@ -0,0 +1,75 @@
+package com.notethonker.notethonker.domain;
+import static com.notethonker.notethonker.common.NoteConstants.EMPTY_NOTE;
+import static com.notethonker.notethonker.common.NoteConstants.INVALID_NOTE;
+import static com.notethonker.notethonker.common.NoteConstants.NOTE;
+// import static org.assertj.core.api.Assertions.hasSize;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.notethonker.notethonker.repository.NoteRepository;
+
+@DataJpaTest
+public class NoteRespositoryTest {
+    @Autowired
+    private NoteRepository noteRepository;
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    @Test
+    public void createNote_WithValidData_ReturnsNote() { 
+        Note note = noteRepository.save(NOTE);
+        Note sut = testEntityManager.find(Note.class, note.getId());
+
+        assertThat(sut).isNotNull();
+        assertThat(sut.getTitle()).isEqualTo(NOTE.getTitle());
+        assertThat(sut.getContent()).isEqualTo(NOTE.getContent());
+    }
+
+    @Test
+    public void createNote_WithInvalidData_ThrowsException() { 
+        assertThatThrownBy(()-> noteRepository.save(EMPTY_NOTE)).isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(()-> noteRepository.save(INVALID_NOTE)).isInstanceOf(RuntimeException.class);
+
+    }
+
+    @Test
+    public void getNote_ByExistingId_ReturnsNote() { 
+        Note note = testEntityManager.persistFlushFind(NOTE);
+        testEntityManager.detach(note);
+
+        Optional<Note> sut = noteRepository.findById(note.getId());
+
+        assertThat(sut).isPresent();
+        assertThat(sut.get()).isEqualTo(note);
+    }
+
+    @Test
+    public void getNote_ByNonExistingId_ReturnsEmpty() { 
+        Optional<Note> sut = noteRepository.findById(999L);
+
+        assertThat(sut).isEmpty();
+    }
+
+    @Test
+    public void removeNote_ByExistingId_ReturnsEmpty() {
+        noteRepository.deleteById(1l);
+        List<Note> sut = noteRepository.findAll();
+
+        assertThat(sut).isNotEmpty();
+        assertThat(sut).hasSize(2);
+    }
+
+    @Test
+    public void removeNot_ByNonExistingId_ThrowsException() {
+        assertThatThrownBy(() -> noteRepository.deleteById(1L)).isInstanceOf(EmptyResultDataAccessException.class);
+    }
+}
